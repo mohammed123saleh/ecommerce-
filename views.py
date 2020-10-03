49d54d0bc9cb3186fcf93bf29a24ed748027e200
Mohammed Saleh Baber <31297109+mohammed123saleh@users.noreply.github.com>
2020-10-03 13:57:42 +0200

    Create views.py

---
+++ views.py
@@ -0,0 +1,94 @@
+from .models import Item, OrderItem, Order, Variation
+
+
+
+@allow_lazy_user
+def add_to_cart(request, slug):
+
+    item = get_object_or_404(Item, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item,
+        user=request.user,
+        ordered=False
+    )
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+
+            messages.info(request, "This item quantity was updated.")
+            return redirect("Main:order-summary")
+        else:
+            order.items.add(order_item)
+            messages.info(request, "This item was added to your cart.")
+            return redirect("Main:order-summary")
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(
+            user=request.user, ordered_date=ordered_date)
+        order.items.add(order_item)
+        messages.info(request, "This item was added to your cart.")
+    return redirect("Main:order-summary")
+
+
+@allow_lazy_user
+def remove_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            order.items.remove(order_item)
+            order_item.delete()
+            
+            messages.info(request, "This item was removed from your cart.")
+            return redirect("Main:order-summary")
+        else:
+            messages.info(request, "This item was not in your cart")
+            return redirect("Main:product", slug=slug)
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect("Main:product", slug=slug)
+
+
+@allow_lazy_user
+def remove_single_item_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+            else:
+                order.items.remove(order_item)
+            messages.info(request, "This item quantity was updated.")
+            return redirect("Main:order-summary")
+        else:
+            messages.info(request, "This item was not in your cart")
+            return redirect("Main:product", slug=slug)
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect("Main:product", slug=slug)
